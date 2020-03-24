f11e8c58d483b80c1724be3b97fe7c9a42cb9f5d
张俊加 <1020446694@qq.com>
2020-03-24 13:01:13 +0800

    chore(config): Configure information to add support for json, yaml, and XML

--- config.go
+++ config.go
@@ -1,6 +1,6 @@
 package fdfs_client
 
 type Configuration struct {
-	Trackers    []string
-	MaxOpenCons int
+	Trackers    []string `json:"trackers" xml:"trackers" yaml:"trackers"`
+	MaxOpenCons int      `json:"max_open_cons" xml:"max_open_cons" yaml:"trackers"`
 }
